7c09d1386d3ee229499dcddd719cb6369c6ca6e2
lowganma <williams.cz1998@gmail.com>
2025-06-09 10:29:20 -0400

    mi primer commit

---
+++ src/components/templates/HomeTemplate.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export function HomeTemplate() {
+  return (
+    <Container>
+      <h1>Homesadasd
+      </h1>
+    </Container>
+  );
+}
+const Container = styled.div`
+display: grid;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: ${props => props.theme.bgtotal};
+color: ${props => props.theme.text};
+width: 100%;
+`
