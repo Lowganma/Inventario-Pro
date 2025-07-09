6d231d91021f7630b5ee16edce35ed7a209d0e57
lowganma <williams.cz1998@gmail.com>
2025-07-08 21:21:55 -0400

    pequeño avanace en la tabla de marcas

--- src/components/organismos/tablas/tablaMarca.jsx
+++ src/components/organismos/tablas/tablaMarca.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {useReactTable} from "@tanstack/react-table"
+import {getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable} from "@tanstack/react-table"
 export function TablaMarca({data}) {
     const columns=[
         {
@@ -9,11 +9,34 @@ export function TablaMarca({data}) {
     const table = useReactTable({
         data,
         columns,
+        getCoreRowModel: getCoreRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        getPaginationRowModel:getPaginationRowModel(),
         
-
     });
     return (<Container>
-        <h1>Componente</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Total Valor</th>
+                    <th>Cantidad</th>
+                    <th>Stock</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        $.12
+                    </td>
+                    <td>
+                       5
+                    </td>
+                    <td>
+                        100
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </Container>);
 }
 const Container = styled.div`
